a60d75d8a121b9c5828dd6b6cb2c68dc2f8d127f
AQP20171679 <alejandraadriantejada@gmail.com>
2017-08-22 16:33:37 -0500

    Muestra los botones al pasar el mouse

--- js/app.js
+++ js/app.js
@@ -31,37 +31,43 @@ for (var i = 0; i < imgProducts.length; i++) {
 	row.appendChild(col_4);
 }
 
+	var atras = document.createElement('button');
+		atras.setAttribute('class', 'atras');
+		atras.setAttribute('onclick','back(this)');
+	var adelante = document.createElement('button');
+		adelante.setAttribute('class', 'adelante');
+		adelante.setAttribute('onclick','next(this)');
+	var btnCancel = document.createElement('button');
+		btnCancel.setAttribute('class','btn-cancel');
+		btnCancel.setAttribute('onclick','borrar(this)');
+
 function big(event){
 		position = img_Products.indexOf(event);
 		pantalla = document.createElement('div'); // es donde se ubicara la imagen
 		pantalla.setAttribute('class','fondo');
-	var atras = document.createElement('button');
-		atras.setAttribute('id', 'atras');
-		atras.setAttribute('onclick','atras(this)');
-	var adelante = document.createElement('button');
-		adelante.setAttribute('id', 'adelante');
-		adelante.setAttribute('onclick','adelante(this)');
 	var galeria = document.createElement('div');
 		galeria.setAttribute('class', 'galeria');
 		imgBig = document.createElement('img'); 
 		imgBig.src ='assets/images/'+imgProducts[position];
 		imgBig.setAttribute('class','img-big');
-	var btnCancel = document.createElement('button');
-		btnCancel.setAttribute('class','btn-cancel');
-		btnCancel.setAttribute('onclick','borrar(this)');
-		galeria.appendChild(btnCancel);
-		galeria.appendChild(atras);
+		galeria.setAttribute('onmouseover','addButtons(this)');		
 		galeria.appendChild(imgBig);
-		galeria.appendChild(adelante);
 		pantalla.appendChild(galeria);
 		products.appendChild(pantalla);
+		btnCancel.style.visibility = "none";
+}
+
+function addButtons(e){
+	e.appendChild(btnCancel);
+	e.appendChild(atras);
+	e.appendChild(adelante);
 }
 
 function borrar(event){
 	products.removeChild(pantalla);
 }
 
-function adelante(e){
+function next(e){
 	if (position >= img_Products.length-1){
 		position = 0;
 	}
@@ -69,7 +75,7 @@ function adelante(e){
 	imgBig.src ='assets/images/'+imgProducts[position];
 }
 
-function atras(e){
+function back(e){
 	if (position <= 0){
 		position = img_Products.length;
 	}
